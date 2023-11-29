bdfb97e07e9b9c314d33f5f4a4bf8828b5346141
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-11-29 17:44:24 +0100

    hello task done

---
+++ hello_world/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * using the printf func
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Welcome to C Programming,\n");
+	return (0);
+}
