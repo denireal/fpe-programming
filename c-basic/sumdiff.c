ef80f819f836d54cd6cd5d65ea72d85584423edf
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-10 20:03:30 +0100

    first task

---
+++ c-basic/sumdiff.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* Given values */
+    int a = 5;
+    int b = 7;
+    int c = 9;
+
+    /* Perform arithmetic operations */
+    int sum = a + b + c;
+    int difference = a - b;
+
+    /* Display the results */
+    printf("Sum: %d\n", sum);
+    printf("Difference: %d\n", difference);
+
+    return (0);
+}
